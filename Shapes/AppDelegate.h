25f165ec1264edae364be4801c2900253b7beb47
Angela Salatyan <angelasalatyan@Angelas-Mac.local>
2016-04-23 03:07:29 -0700

    Initial Commit

---
+++ Shapes/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Shapes
+//
+//  Created by Angela Salatyan on 4/23/16.
+//  Copyright (c) 2016 Angela Salatyan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Shapes/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Shapes
+//
+//  Created by Angela Salatyan on 4/23/16.
+//  Copyright (c) 2016 Angela Salatyan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
